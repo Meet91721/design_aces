ab7caab18beec7ed96911865b6be359f0ee1634a
Meet91721 <ma91721@gmail.com>
2021-12-30 07:58:30 +0530

    HomePage Done

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,6 +1,8 @@
-import 'package:design_aces/screens/home/widgets/drawer.dart';
-import 'package:design_aces/screens/home/widgets/responsive.dart';
-import 'package:design_aces/screens/home/widgets/topbar_content.dart';
+// import 'package:design_aces/screens/home/widgets/contact_us.dart';
+import 'package:design_aces/screens/widgets/contact_us.dart';
+import 'package:design_aces/screens/widgets/drawer.dart';
+import 'package:design_aces/screens/widgets/responsive.dart';
+import 'package:design_aces/screens/widgets/topbar_content.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -38,7 +40,7 @@ class _HomeState extends State<Home> {
       appBar: Responsive.isSmallScreen(context)
           ? AppBar(
               backgroundColor: Colors.blueGrey.shade900
-                  .withOpacity(_opacity < 0.1 ? 0.1 : _opacity),
+                  .withOpacity(_opacity < 0.6 ? 0.6 : _opacity),
               elevation: 0,
               title: Text(
                 'ACES',
@@ -54,7 +56,7 @@ class _HomeState extends State<Home> {
           : PreferredSize(
               preferredSize: Size(screenSize.width, 1000),
               child: TopBarContent(
-                opacity: _opacity < 0.55 ? 0.6 : _opacity,
+                opacity: _opacity < 0.6 ? 0.6 : _opacity,
               ),
             ),
       drawer: const ExploreDrawer(),
@@ -63,17 +65,80 @@ class _HomeState extends State<Home> {
           physics: const ClampingScrollPhysics(),
           child: Column(
             children: [
-              Stack(
-                children: [
-                  Container(
-                      child: SizedBox(
-                    height: screenSize.height * 0.65,
-                    width: screenSize.width,
-                    child: Image.asset('assets/images/Home1.jpg',
-                        fit: BoxFit.cover),
-                  ))
-                ],
-              )
+              SizedBox(
+                height: screenSize.height * 0.60,
+                width: screenSize.width,
+                child:
+                    Image.asset('assets/images/Home1.jpg', fit: BoxFit.cover),
+              ),
+              const SizedBox(
+                height: 29,
+              ),
+              const Padding(
+                padding: EdgeInsets.fromLTRB(70, 16, 70, 16),
+                child: Center(
+                  child: Text(
+                    'Association of Computer Engineering Students (ACES), was established in 2001 by the Department of Computer Science and Engineering. All the students of the Department are members of ACES. ACES aims to make students technically stronger by organising various workshops and seminars on the latest technologies. The organisation also contributes to the betterment of society by organising socially relevant technical activities.',
+                    textAlign: TextAlign.justify,
+                    style: TextStyle(
+                        color: Colors.blueGrey,
+                        fontFamily: 'Montserrat',
+                        fontSize: 20),
+                  ),
+                ),
+              ),
+              Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: const [
+                    Image(
+                      image: AssetImage('assets/images/logo/ACES.png'),
+                      fit: BoxFit.contain,
+                      height: 200,
+                      width: 200,
+                    ),
+                    Image(
+                      image: AssetImage('assets/images/logo/NU.png'),
+                      height: 200,
+                      width: 200,
+                      fit: BoxFit.contain,
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 29,
+              ),
+              const Padding(
+                padding: EdgeInsets.fromLTRB(70, 16, 70, 16),
+                child: Center(
+                  child: Text(
+                    'ACES aims to make students technically stronger by organising various workshops and seminars on the latest technologies. The organisation also contributes to the betterment of society by organising socially relevant technical activities.',
+                    textAlign: TextAlign.justify,
+                    style: TextStyle(
+                        color: Colors.blueGrey,
+                        fontFamily: 'Montserrat',
+                        fontSize: 20),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 29,
+              ),
+              const Padding(
+                padding: EdgeInsets.fromLTRB(70, 16, 70, 16),
+                child: Center(
+                  child: Text(
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+                    textAlign: TextAlign.justify,
+                    style: TextStyle(
+                        color: Colors.blueGrey,
+                        fontFamily: 'Montserrat',
+                        fontSize: 20),
+                  ),
+                ),
+              ),
+              const ContactUs(),
             ],
           )),
     );
---
+++ lib/screens/widgets/contact_us.dart
@@ -0,0 +1,266 @@
+import 'package:design_aces/screens/widgets/responsive.dart';
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class ContactUs extends StatelessWidget {
+  const ContactUs({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Future<void> _launchInBrowser(String url) async {
+      if (!await launch(
+        url,
+        forceSafariVC: false,
+        forceWebView: false,
+        headers: <String, String>{'my_header_key': 'my_header_value'},
+      )) {
+        throw 'Could not launch $url';
+      }
+    }
+
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      color: Colors.blueGrey[900],
+      child: Responsive.isSmallScreen(context)
+          ? Column(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 20.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        '\nConnect us on:\n',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.blueGrey[300],
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          _launchInBrowser(lI);
+                        },
+                        child: Text(
+                          'LinkedIn',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Colors.blueGrey[100],
+                            fontSize: 14,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 5),
+                      GestureDetector(
+                        onTap: () {
+                          _launchInBrowser(iG);
+                        },
+                        child: Text(
+                          'Instagram',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Colors.blueGrey[100],
+                            fontSize: 14,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      GestureDetector(
+                        onTap: () {
+                          _launchInBrowser(fb);
+                        },
+                        child: Text(
+                          'Facebook',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Colors.blueGrey[100],
+                            fontSize: 14,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const Divider(
+                  color: Colors.blueGrey,
+                ),
+                const SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 20.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Visit us at:\n',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.blueGrey[300],
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          _launchInBrowser(wB);
+                        },
+                        child: Text(
+                          'Official Website',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Colors.blueGrey[100],
+                            fontSize: 14,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      Text(
+                        'Nirma University, Ahmedabad',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.blueGrey[100],
+                          fontSize: 14,
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                    ],
+                  ),
+                ),
+              ],
+            )
+          : Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 20.0, top: 16),
+                  child: Column(
+                    // crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Contact Us:',
+                        style: TextStyle(
+                          color: Colors.blueGrey[300],
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          _launchInBrowser(lI);
+                        },
+                        child: Text(
+                          'LinkedIn',
+                          style: TextStyle(
+                            color: Colors.blueGrey[100],
+                            fontSize: 14,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      GestureDetector(
+                        onTap: () {
+                          _launchInBrowser(iG);
+                        },
+                        child: Text(
+                          'Instagram',
+                          style: TextStyle(
+                            color: Colors.blueGrey[100],
+                            fontSize: 14,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      GestureDetector(
+                        onTap: () {
+                          _launchInBrowser(fb);
+                        },
+                        child: Text(
+                          'FaceBook',
+                          style: TextStyle(
+                            color: Colors.blueGrey[100],
+                            fontSize: 14,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  color: Colors.blueGrey,
+                  width: 4,
+                  height: 150,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Column(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Visit Us:',
+                          style: TextStyle(
+                            color: Colors.blueGrey[300],
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            _launchInBrowser(wB);
+                          },
+                          child: Text(
+                            'Website',
+                            style: TextStyle(
+                              color: Colors.blueGrey.shade300,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ),
+                        Row(
+                          mainAxisSize: MainAxisSize.min,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Address: ',
+                              style: TextStyle(
+                                color: Colors.blueGrey.shade300,
+                                fontSize: 16,
+                              ),
+                            ),
+                            Flexible(
+                              child: Text(
+                                'Nirma University, Ahmedabad',
+                                style: TextStyle(
+                                  color: Colors.blueGrey.shade100,
+                                  fontSize: 16,
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                )
+              ],
+            ),
+    );
+  }
+
+  final String lI =
+      'https://www.linkedin.com/company/association-of-computer-engineering-students/';
+  final String iG = 'https://www.instagram.com/aces_it_nu/';
+  final String wB = 'https://technology.nirmauni.ac.in/student_work/aces/';
+  final String fb = 'https://www.facebook.com/aces.it.itnu/';
+}
